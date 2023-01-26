d7599322bf06374d7aaea71371ddb38b14bb4496
Paulo <paulofs10.sea@gmail.com>
2023-01-26 12:28:33 -0300

    player xp and mana manager

--- Legends Of Conquest 2D RPG/Assets/Scripts/Charaters/PlayerStats.cs
+++ Legends Of Conquest 2D RPG/Assets/Scripts/Charaters/PlayerStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,11 @@ public class PlayerStats : MonoBehaviour
 {
     [SerializeField] string playerName;
 
+    [SerializeField] int maxLevel = 50;
     [SerializeField] int playerLevel = 1;
     [SerializeField] int currentXP;
+    
+    [SerializeField] int baseLevelXp = 100;
 
     [SerializeField] int maxHP=100;
     [SerializeField] int currentHP;
@@ -18,14 +22,49 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] int dexterity;
     [SerializeField] int defence;
 
+    [SerializeField] int[] xpForEachLevel;
     void Start()
     {
-        
+        xpForEachLevel = new int[maxLevel];
+        xpForEachLevel[1] = baseLevelXp;
+
+        for(int i = 2; i < xpForEachLevel.Length; i++)
+        {
+            xpForEachLevel[i] = (int)(0.02f * i * i + 3.06f * i * i + 105.6f * i);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            AddXP(100);
+        }
+    }
+
+    public void AddXP(int amountXp)
+    {
+        currentXP += amountXp;
+        if(currentXP > xpForEachLevel[playerLevel])
+        {
+            currentXP -= xpForEachLevel[playerLevel];
+            playerLevel++;
+
+            if(playerLevel % 2  == 0)
+            {
+                dexterity++;
+            }
+            else
+            {
+                defence++;
+            }
+
+            maxHP = Mathf.FloorToInt(maxHP * 1.18f);
+            currentHP = maxHP;
+
+            maxMana = Mathf.FloorToInt(maxMana * 1.06f);
+            currentMana = maxMana;
+        }
     }
 }
