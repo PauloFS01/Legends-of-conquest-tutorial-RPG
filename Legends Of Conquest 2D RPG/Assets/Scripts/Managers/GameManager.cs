ec90f96afde3e75f3b4a9e78e3d41c25d1a514eb
Paulo <paulofs10.sea@gmail.com>
2023-03-15 10:40:35 -0300

    playerprefs items manager config

--- Legends Of Conquest 2D RPG/Assets/Scripts/Managers/GameManager.cs
+++ Legends Of Conquest 2D RPG/Assets/Scripts/Managers/GameManager.cs
@@ -29,7 +29,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameMenuOpened || dialogBoxOpened || shopOpened)
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Data has been saved");
+            SaveData();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("Data has been load");
+            LoadData();
+        }
+
+        if (gameMenuOpened || dialogBoxOpened || shopOpened)
         {
             Player.instance.deactivateMovement = true;
         }else
@@ -41,5 +54,82 @@ public class GameManager : MonoBehaviour
     public PlayerStats[] GetPlayerStats()
     {
         return playerStats;
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetFloat("Player_Pos_X", Player.instance.transform.position.x);
+        PlayerPrefs.SetFloat("Player_Pos_Y", Player.instance.transform.position.y);
+        PlayerPrefs.SetFloat("Player_Pos_Z", Player.instance.transform.position.z);
+        
+        for(int i = 0; i < playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy)
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_active", 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_active",0);
+            }
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_Level", playerStats[i].playerLevel);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_CurrentXP", playerStats[i].currentXP);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_MaxHP", playerStats[i].maxHP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_CurrentHP", playerStats[i].currentHP);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_MaxMana", playerStats[i].maxMana);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_CurrentMana", playerStats[i].currentMana);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_Dexterity", playerStats[i].dexterity);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_Defence", playerStats[i].defence);
+
+            PlayerPrefs.SetString("Player_" + playerStats[i].playerName + "_EquipedWeapon", playerStats[i].equipedWeaponName);
+            PlayerPrefs.SetString("Player_" + playerStats[i].playerName + "_EquipedArmor", playerStats[i].equipedArmorName);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_WeaponPower", playerStats[i].weaponPower);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].playerName + "_ArmorDefence", playerStats[i].armorDefence);
+
+        }
+    }
+
+    public void LoadData()
+    {
+        Player.instance.transform.position = new Vector3(
+            PlayerPrefs.GetFloat("Player_Pos_X"),
+            PlayerPrefs.GetFloat("Player_Pos_Y"),
+            PlayerPrefs.GetFloat("Player_Pos_Z")
+            );
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if(PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_active") == 0)
+            {
+                playerStats[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                playerStats[i].gameObject.SetActive(true);
+            }
+
+            playerStats[i].playerLevel = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_Level");
+            playerStats[i].currentXP = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_CurrentXP");
+
+            playerStats[i].maxHP = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_MaxHP");
+            playerStats[i].currentXP = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_CurrentHP");
+
+            playerStats[i].maxMana = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_MaxMana");
+            playerStats[i].currentMana = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_CurrentMana");
+
+            playerStats[i].dexterity = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_Dexterity");
+            playerStats[i].defence = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_Defence");
+
+            playerStats[i].equipedWeaponName = PlayerPrefs.GetString("Player_" + playerStats[i].playerName + "_EquipedWeapon");
+            playerStats[i].equipedArmorName = PlayerPrefs.GetString("Player_" + playerStats[i].playerName + "_EquipedArmor");
+
+            playerStats[i].weaponPower = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_WeaponPower");
+            playerStats[i].armorDefence = PlayerPrefs.GetInt("Player_" + playerStats[i].playerName + "_ArmorDefence");
+        }
     }
 }
