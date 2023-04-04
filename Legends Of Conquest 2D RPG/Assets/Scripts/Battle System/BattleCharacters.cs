99d120897bb398f2cd75b2c81eee3ac5b68e96fa
Paulo <paulofs10.sea@gmail.com>
2023-04-04 10:25:31 -0300

    create battle scene and enemies

---
+++ Legends Of Conquest 2D RPG/Assets/Scripts/Battle System/BattleCharacters.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCharacters : MonoBehaviour
+{
+    [SerializeField] bool isPlayer;
+    [SerializeField] string[] attacksAvaiable;
+
+    public string characterName;
+    public int currentHP, maxHP, currentMana, dexterity, defence, wpnPower, armorDefence;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
