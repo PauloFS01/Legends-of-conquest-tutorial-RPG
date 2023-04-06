d77135cb217ccb774c3397389190f1b90dad188a
Paulo <paulofs10.sea@gmail.com>
2023-04-06 11:19:01 -0300

    battle system spaw

--- Legends Of Conquest 2D RPG/Assets/Scripts/Battle System/BattleManager.cs
+++ Legends Of Conquest 2D RPG/Assets/Scripts/Battle System/BattleManager.cs
@@ -8,7 +8,10 @@ public class BattleManager : MonoBehaviour
     private bool isBattleActive;
 
     [SerializeField] GameObject battleScene;
+    [SerializeField] List<BattleCharacters> activeCharacters = new List<BattleCharacters>();
+
     [SerializeField] Transform[] playerPositions, enemiesPositions;
+
     [SerializeField] BattleCharacters[] playerPrefabs, enemyPrefabs;
 
 
@@ -29,6 +32,73 @@ public class BattleManager : MonoBehaviour
 
 
     public void StartBattle(string[] enemiesToSpaw)
+    {
+        SettingBatle();
+        AddingPlayers();
+
+        for(int i =0; i < enemiesToSpaw.Length; i++)
+        {
+            if (enemiesToSpaw[i] != "")
+            {
+                for(int j = 0; j < enemyPrefabs.Length; j++)
+                {
+                    BattleCharacters newEnemy = Instantiate(
+                        enemyPrefabs[i],
+                        enemiesPositions[i].position,
+                        enemiesPositions[i].rotation,
+                        enemiesPositions[i]
+                        );
+
+                    activeCharacters.Add(newEnemy);
+                }
+            }
+        }
+    }
+
+    private void AddingPlayers()
+    {
+        for (int i = 0; i < GameManager.instance.GetPlayerStats().Length; i++)
+        {
+            if (GameManager.instance.GetPlayerStats()[i].gameObject.activeInHierarchy)
+            {
+                for (int j = 0; j < playerPrefabs.Length; j++)
+                {
+                    if (playerPrefabs[j].characterName == GameManager.instance.GetPlayerStats()[i].playerName)
+                    {
+                        BattleCharacters newPlayer = Instantiate(
+                            playerPrefabs[j],
+                            playerPositions[i].position,
+                            playerPositions[i].rotation,
+                            playerPositions[i]
+                            );
+
+                        activeCharacters.Add(newPlayer);
+                        ImportPlayerStats(i);
+
+                    }
+                }
+            }
+        }
+    }
+
+    private void ImportPlayerStats(int i)
+    {
+        PlayerStats player = GameManager.instance.GetPlayerStats()[i];
+
+        activeCharacters[i].currentHP = player.currentHP;
+        //activeCharacters[i].maxHp = player.maxHp;
+
+        activeCharacters[i].currentMana = player.currentHP;
+        //activeCharacters[i].maxMana = player.maxMana;
+
+        activeCharacters[i].dexterity = player.dexterity;
+        activeCharacters[i].defence = player.defence;
+
+        //activeCharacters[i].wpnPower = player.wpnPower;
+        activeCharacters[i].armorDefence = player.armorDefence;
+    }
+
+    private void SettingBatle()
     {
         if (!isBattleActive)
         {
